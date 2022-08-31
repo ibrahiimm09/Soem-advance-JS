dbb76a580ca82d436a2c70e2554799bc47847bd1
Muhammad Ibrahim <ia4bwp@gmail.com>
2022-08-31 12:23:09 +0500

    Fetched JSON

--- fetch.js
+++ fetch.js
@@ -3,3 +3,30 @@ function getText(){
     fetch('sample.txt').then(res => {return res.text()}).then(data => document.getElementById('demo').innerHTML = data);
 }
 document.getElementById('txtBtn').addEventListener('click', getText)
+
+
+// JSON:
+function getUsers(){
+    
+    fetch('sample.json').then(res => {return res.json()})
+    .then(data => {
+
+        // console.log(data);
+        let output = '<h2>Users</h2>';
+
+        data.forEach(user => {
+            console.log(user.id);
+            output += 
+            `<ul>
+                <li>ID: ${user.id}</li>
+                <li>Name: ${user.name}</li>
+                <li>E-mail: ${user.email}</li>
+            </ul>`;
+        });
+
+        document.getElementById('demo1').innerHTML = output;
+        
+        })
+}
+
+document.getElementById('jsonBtn').addEventListener('click', getUsers)
