d44ef3ed7479b2e27dd0ace3029b5cad995b180a
Muhammad Ibrahim <ia4bwp@gmail.com>
2022-08-31 22:08:14 +0500

    Fetched API

--- fetch.js
+++ fetch.js
@@ -24,9 +24,55 @@ function getUsers(){
             </ul>`;
         });
 
-        document.getElementById('demo1').innerHTML = output;
+        document.getElementById('demo2').innerHTML = output;
         
         })
 }
 
 document.getElementById('jsonBtn').addEventListener('click', getUsers)
+
+// API:
+function getPosts(){
+    
+    fetch('https://jsonplaceholder.typicode.com/posts').then(res => {return res.json()})
+    .then(data => {
+
+        // console.log(data);
+        let output = '<h2>Posts</h2>';
+
+        data.forEach(posts => {
+            // console.log(posts);
+            output += 
+            `<div>
+                <h4>${posts.title}</h4>
+                <h6>${posts.body}</h6>
+            </div>`;
+        });
+
+        document.getElementById('demo3').innerHTML = output;
+        
+        })
+}
+
+document.getElementById('apiBtn').addEventListener('click', getPosts)
+
+
+
+document.getElementById('addPosts').addEventListener('submit', addPost)
+function addPost(e){
+    e.preventDefault();
+    let mytitle = document.getElementById('title').value;
+    console.log(mytitle);
+    let mybody = document.getElementById('body').value;
+    console.log(mybody);
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method : 'POST',
+        header:{
+            'Accept' : 'application/json, text/plain, */*',
+            'Content-type' : 'application/json',
+        },
+        body: JSON.stringify({title : mytitle , body : mybody})
+    })
+    .then(res => {return res.json()})
+    .then(data =>   console.log(data))
+}
--- promise.js
+++ promise.js
@@ -75,7 +75,7 @@ const Promise3 = new Promise((resolve , reject)=>{
 // 'res' stores the value return by .then on fulfilment of promise.
 const Promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 
-// Here 'values' stores the va;ues returned y .then on fulfilment of promise.
+// Here 'values' stores the values returned by .then on fulfilment of promise.
 Promise.all([Promise1 , Promise2 , Promise3, Promise4]).then(values => console.log(values))
 
 // The Promise.all() method takes an iterable of promises as an input, and returns a single Promise that resolves to an array of the results of the input promises. This returned promise will fulfill when all of the input's promises have fulfilled, or if the input iterable contains no promises. It rejects immediately upon any of the input promises rejecting or non-promises throwing an error, and will reject with this first rejection message / error.
